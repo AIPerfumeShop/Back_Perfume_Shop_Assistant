00b1c7326ae4fb74c27b55ef790574782d4fc490
somathMORM <samathmorm123@gmail.com>
2026-08-23 15:53:50 +0700

    fixed: config

--- src/main/java/com/example/spring_boot_project_api/config/SecurityConfig.java
+++ src/main/java/com/example/spring_boot_project_api/config/SecurityConfig.java
@@ -8,18 +8,21 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-        .sessionManagement(session -> 
-            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        )
-        .csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth -> auth
-            .requestMatchers("/api/auth/**")
-            .permitAll()
-            .anyRequest().authenticated()
-        );
+            .sessionManagement(session ->
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/auth/**").permitAll()
+                .requestMatchers("/swagger-ui/**").permitAll()
+                .requestMatchers("/swagger-ui.html").permitAll()
+                .requestMatchers("/v3/api-docs/**").permitAll()
+                .anyRequest().authenticated()
+            );
         return http.build();
     }
 }
