53304fa82f54b6d3297d3375a0c2233bab375987
Cherry272007 <chansovannary27@gmail.com>
2026-08-24 16:44:35 +0700

    Add env dependency config

--- src/main/java/com/example/spring_boot_project_api/SpringBootProjectApiApplication.java
+++ src/main/java/com/example/spring_boot_project_api/SpringBootProjectApiApplication.java
@@ -3,10 +3,17 @@ package com.example.spring_boot_project_api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class SpringBootProjectApiApplication {
 
 	public static void main(String[] args) {
+		// Load .env into System properties so Spring Boot can replace placeholders like ${DB_PORT}
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
 		SpringApplication.run(SpringBootProjectApiApplication.class, args);
 	}
 
