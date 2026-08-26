b29350f2629b3f839d49b5edf0ef2f6892e6ad0d
Cherry272007 <chansovannary27@gmail.com>
2026-08-26 16:42:18 +0700

    ProductResponse DTO

--- src/main/java/com/example/spring_boot_project_api/dto/response/product/ProductResponse.java
+++ src/main/java/com/example/spring_boot_project_api/dto/response/product/ProductResponse.java
@@ -1,5 +1,27 @@
 package com.example.spring_boot_project_api.dto.response.product;
 
-public class ProductResponse {
+import java.util.List;
 
+import com.example.spring_boot_project_api.enums.Gender;
+
+import lombok.Data;
+
+@Data
+public class ProductResponse {
+    private Long id;
+    private String name;
+    private String brand;
+    private String description;
+    private Long category_id;
+    private String category_name;
+    private List<ProductVariantResponse> variant;
+    private Gender gender;
+    private String fragrance_family;
+    private String frag_notes;
+    private String intensity;
+    private List<String> images;
+    private Double averageRate;
+    private Integer reviewCount;
+    private Boolean inStock;
+    private Boolean active;
 }
