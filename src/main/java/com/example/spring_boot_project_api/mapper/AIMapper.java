81871ec87688d63a2fd81fa33f661f9cd1cfc2df
Cherry272007 <chansovannary27@gmail.com>
2026-08-26 23:36:08 +0700

    AIcon Mapper

--- src/main/java/com/example/spring_boot_project_api/mapper/AIMapper.java
+++ src/main/java/com/example/spring_boot_project_api/mapper/AIMapper.java
@@ -1,5 +1,40 @@
 package com.example.spring_boot_project_api.mapper;
 
-public class AIMapper {
+import org.springframework.stereotype.Component;
 
+import com.example.spring_boot_project_api.dto.response.ai.AIConversationResponse;
+import com.example.spring_boot_project_api.dto.response.ai.AIMessageResponse;
+import com.example.spring_boot_project_api.model.AIConversation;
+import com.example.spring_boot_project_api.model.AIMessage;
+
+@Component
+public class AIMapper {
+    public AIConversationResponse toConversationResponse(AIConversation conversation){
+        if(conversation == null){
+            return null;
+        }
+        AIConversationResponse response = new AIConversationResponse();
+        response.setId(conversation.getId());
+        response.setTitle(conversation.getTitle());
+        response.setCreatedAt(conversation.getCreatedAt());
+        response.setUpdatedAt(conversation.getUpdatedAt());
+
+        if(conversation.getUser() != null){
+            response.setUserId(conversation.getUser().getId());
+        }
+        return response;
+    }
+    public AIMessageResponse toMessageResponse(AIMessage message){
+        if(message == null){
+            return null;
+        }
+        AIMessageResponse response = new AIMessageResponse();
+        response.setId(message.getId());
+        response.setSender(message.getSender());
+        response.setMessage(message.getMessage());
+        response.setCreatedAt(message.getCreatedAt());
+        response.setUpdatedAt(message.getUpdatedAt());
+
+        return response;
+    }
 }
