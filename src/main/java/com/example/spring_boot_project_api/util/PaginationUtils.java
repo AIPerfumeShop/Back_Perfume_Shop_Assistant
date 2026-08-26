fe91e353fa9acc67a8d382495a9539f469c71770
Cherry272007 <chansovannary27@gmail.com>
2026-08-26 15:58:08 +0700

    feature PaginationUtils

--- src/main/java/com/example/spring_boot_project_api/util/PaginationUtils.java
+++ src/main/java/com/example/spring_boot_project_api/util/PaginationUtils.java
@@ -1,5 +1,23 @@
 package com.example.spring_boot_project_api.util;
 
-public class PaginationUtils {
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+public final class PaginationUtils {
+    private static final int DEFAULT_PAGE = 0;
+    private static final int DEFAULT_SIZE = 10;
+    private static final int MAX_SIZE = 100;
+    private static final String DEFAULT_SORT = "createdAt";
+    public PaginationUtils(){
+        //prevent object creation
+    }
+    public static Pageable createPageable(Integer page, Integer size, String sortby, String direction){
+        int pageNumber = (page == null || page < 0) ? DEFAULT_PAGE : page;
+        int pageSize = (size == null || size <= 0) ? DEFAULT_SIZE : Math.min(size, MAX_SIZE);
+        String sortField = (sortby == null || sortby.isBlank()) ? DEFAULT_SORT : sortby;
+        Sort.Direction sortDirection = "asc".equalsIgnoreCase(direction) ? Sort.Direction.ASC : Sort.Direction.DESC;
+        
+        return PageRequest.of(pageNumber, pageSize,Sort.by(sortDirection,sortField));
+    }
 }
