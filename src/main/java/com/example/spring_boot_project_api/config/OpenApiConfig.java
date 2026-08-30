0e9246e8e0af9c4e0d34af40ebb668ce9c685d3a
Cherry272007 <chansovannary27@gmail.com>
2026-08-30 15:44:43 +0700

    OpenAPIconfig

--- src/main/java/com/example/spring_boot_project_api/config/OpenApiConfig.java
+++ src/main/java/com/example/spring_boot_project_api/config/OpenApiConfig.java
@@ -1,5 +1,20 @@
 package com.example.spring_boot_project_api.config;
 
-public class OpenApiConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customerOpenAPI(){
+        return new OpenAPI()
+        .info(new Info()
+                .title("Blossom Fragrance API")
+                .version("1.0.0")
+                .description("API for Blossom Fragrance Perfume Shop")
+    );
+    }
 }
